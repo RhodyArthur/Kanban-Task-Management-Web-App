e05ceecc29809e00db73ad0f7180e0c3a71ce835
IT Compliance <rhodaarthur48@gmail.com>
2024-09-23 23:29:22 -0700

    updates completed tasks count

--- src/app/services/board.service.ts
+++ src/app/services/board.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Store, select } from '@ngrx/store';
-import { BehaviorSubject,  Observable } from 'rxjs';
+import {BehaviorSubject, Observable, take} from 'rxjs';
 import { Board } from '../models/board';
-import { selectAllBoards } from '../state/board/board.selectors';
-import { setSelectedBoard } from '../state/board/board.actions';
+import {selectAllBoards, selectSelectedBoard} from '../state/board/board.selectors';
+import { setSelectedBoard, updateBoard } from '../state/board/board.actions';
+import {Task} from "../models/task";
 
 @Injectable({
   providedIn: 'root'
@@ -24,4 +25,30 @@ export class BoardService {
     this.store.dispatch(setSelectedBoard({board}));
   }
 
+    // update board
+    updateBoard(board: Board) {
+        this.store.dispatch(updateBoard({ board }));
+        this.store.dispatch(setSelectedBoard({board}));
+    }
+
+    // update task
+    updateTask(updatedTask: Task) {
+        this.store
+            .select(selectSelectedBoard)
+            .pipe(take(1))
+            .subscribe((board) => {
+                if (board) {
+                    const updatedBoard = {
+                        ...board,
+                        columns: board.columns.map((column) => ({
+                            ...column,
+                            tasks: column.tasks.map((task) =>
+                                task.title === updatedTask.title ? updatedTask : task
+                            ),
+                        })),
+                    };
+                    this.updateBoard(updatedBoard);
+                }
+            });
+    }
 }
